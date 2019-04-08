b7d0df3261129c12295999f90601ca2003b98194
Prerna <36573566+IMCHOUDHARY@users.noreply.github.com>
2019-04-08 20:47:50 +0530

    Commit4

---
+++ forms.blade.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<style>
+body{
+	margin:0px;
+	padding:0px;
+}
+/*#background{
+background-color:#F0F0F0;
+width:100%;
+height:100%;
+}
+#start{
+	width:100%;
+	height:40px;
+	background-color:#E0E0E0;
+}*/
+</style>
+<script>
+
+</script>
+	</head>
+	<body>
+    <!--<div id="background">
+
+     <div id="start">
+
+     </div>
+
+
+    </div>-->
+   <div class="row">
+  <div class="col-md-6" style="background-color:#E0E0E0; height:40px;">
+  	&nbsp &nbsp &nbsp &nbsp 
+  	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+  	&nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp
+  	  	<input type="text" name="name" placeholder="Enter Your Email-Id" style=" margin:5px;width:300px;">
+  	<button type="button" class="btn btn-success" style="height:30px; font-style:italic;">Subscribe</button>
+     </div>
+  	 <div class="col-md-6" style="background-color:#E0E0E0; height:40px;">
+  	 	&nbsp &nbsp &nbsp &nbsp 
+  	 	&nbsp &nbsp &nbsp &nbsp 
+        &nbsp &nbsp &nbsp &nbsp 
+        &nbsp &nbsp &nbsp &nbsp 
+        &nbsp &nbsp &nbsp &nbsp 
+  	 	&nbsp &nbsp &nbsp &nbsp 
+  	 	<i class="fa fa-facebook-official" style="font-size:28px; margin:5px;"></i>
+  	 	<i class="fa fa-twitter-square" style="font-size:28px"></i>
+        <i class="fa fa-youtube-square" style="font-size:28px"></i>
+        <i class="fa fa-linkedin-square" style="font-size:28px"></i>
+        <i class="fa fa-google-plus-square" style="font-size:28px"></i>
+        &nbsp &nbsp &nbsp &nbsp 
+  	 	&nbsp &nbsp &nbsp &nbsp 
+        <button type="button" class="btn btn-success" style="height:50px width:60px; font-style:italic; margin:5px; 
+        background-color:yellow; color:black;"><b>Donate Now</b></button></div>
+    </div>
+
+    <div class="row">
+    <div class="col-md-3" style="background-color:white; height:100px;">
+    &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp &nbsp &nbsp
+    &nbsp &nbsp <img src="smiley.jpg" width='130px' height='100px'></div>
+
+    <div class="col-md-9" style="background-color:white; height:100px;">
+    <nav class="navbar navbar-default" style="margin:49px; width:100%;">
+  <div class="container-fluid" style="background-color:white;">
+    <!--<div class="navbar-header" >
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>-->
+    <ul class="nav navbar-nav" style="background-color:white;">
+      <li class="active" style="width:150px;"><a href="#">Home</a></li>
+      <li style="width:150px;"><a href="#">About us</a></li>
+      <li style="width:150px;"><a href="form">Contact us</a></li>
+      <li style="width:150px;"><a href="#">Compaign</a></li>
+      <li style="width:150px;"><a href="#">Our Work</a></li>
+    </ul>
+  </div>
+</nav></div>
+    </div>
